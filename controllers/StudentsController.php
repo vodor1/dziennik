d39a01c430a5404c090a43486f8edf87740a2c21
vodor1 <wo.kucharczyk@u.zs3-wyszkow.pl>
2025-11-25 09:40:58 +0100

    25.11.2025 moja ciężka praca

--- controllers/StudentsController.php
+++ controllers/StudentsController.php
@@ -67,7 +67,16 @@ require_once "entities\Student.php";
 
             $db_connection = mysqli_connect("localhost", "root", "", "database");
             $query = mysqli_query($db_connection,
-                "SELECT * FROM `database`.students WHERE id = '{$_GET['student_id']}'");
+                "
+                        SELECT 
+                            students.id,
+                            students.name,
+                            students.surname,
+                            users.email
+                                FROM `database`.students 
+                                    JOIN `database`.users 
+                                    ON `database`.users.`id` = `database`.students.`user_id`
+                                    WHERE `database`.students.`id` = '{$_GET['student_id']}'");
             $student = mysqli_fetch_assoc($query);
             include("templates/student/edit.php");
         }
